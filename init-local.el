dc92b11380711983c5ff9eb40565ea49aa91995a
yousen <zysright@gmail.com>
2021-01-18 23:10:51 -0600

    small update for emacs customization

--- init-local.el
+++ init-local.el
@@ -1,9 +1,14 @@
-;;; init-local.el --- My own custom file -*- lxical-binding: t -*-
+;;; init-local.el --- My own custom file -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
+;;; my favoriate color theme
 (require-package 'color-theme-sanityinc-tomorrow)
 (setq-default custom-enabled-themes '(sanityinc-tomorrow-day))
+
+;;; org-noter stuffs
+(require-package 'pdf-tools)
+(require-package 'org-noter-pdftools)
 
 (provide 'init-local)
 ;;; init-local.el ends here
