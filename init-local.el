238e7456769272b8459d552a8dc79988f598f4e3
yszhang95 <zysright@gmail.com>
2021-01-18 20:21:33 -0600

    new init-local.el for emacs

---
+++ init-local.el
@@ -0,0 +1,9 @@
+;;; init-local.el --- My own custom file -*- lxical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require-package 'color-theme-sanityinc-tomorrow)
+(setq-default custom-enabled-themes '(sanityinc-tomorrow-day))
+
+(provide 'init-local)
+;;; init-local.el ends here
